f61bce1875b9013e0000d637d4e32376fba8fd90
Edward-moli <edward15moli@gmail.com>
2023-02-11 00:04:08 -0400

    parte faltante del cap 3

---
+++ capitulo 4/4,1modificaciion clase.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/* Cubo-1.
+El programa calcula el cubo de los 10 primeros números naturales con la
+ayuda de una función. En la solución del problema se utiliza una variable
+global, aunque esto, como veremos más adelante, no es muy recomendable. */
+
+int cubo(int); 
+int sumain(int a, int b)   /*Prototipo de funcion.*/
+            
+void main (void)
+{
+    int I;
+    int B;
+    int r;
+ int CUB;
+ for ( I = 1; B=100;I <=10; I++, B--)
+ {
+I=sumain(I,B);
+CUB= cubo(I);       /* Llamada a la funcion cubo. */
+printf("\n El cubo de %i es: %i", I, CUB);
+printf("\n El suma de %i mas %i es: %i", I, CUB);
+ }
+}
+int cubo (int a)   /* Declaración de la función. */
+/* La función calcula el cubo de la variable global I. */
+{
+    return(a*a*a);
+}
+int Sumain(int a,int b )
+{
+    return a+b;
+}
